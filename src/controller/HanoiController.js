df8a1bdabca1c55379e92faa146c093f7cd832ab
Nitin_kumar <82011104+Nitin-094@users.noreply.github.com>
2025-10-23 18:25:38 +0530

    Update HanoiController.js
    updating levels in js

--- src/controller/HanoiController.js
+++ src/controller/HanoiController.js
@@ -1,4 +1,4 @@
-export const levels = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+export const levels = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export function getData(blocks, columns = 3) {
     const data = { blocks: {}, containers: [] };
